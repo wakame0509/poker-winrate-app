a20f2a2257da38df3c1e95877908b66457cbbc7f
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-05-12 15:05:02 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,41 +1,23 @@
-import random
+from treys import Card
+import itertools
 
-RANK_ORDER = '23456789TJQKA'
 
-def card_value(card):
-    return RANK_ORDER.index(card[0])
+def convert_card_str_to_int(card_str):
+    return Card.new(card_str)
 
-def parse_card(card_str):
-    rank = card_str[0]
-    suit = card_str[1]
-    return rank + suit
 
-def generate_deck():
-    suits = ['s', 'h', 'd', 'c']
-    ranks = '23456789TJQKA'
-    return [r + s for r in ranks for s in suits]
+def get_deck_minus_known_cards(known_cards):
+    full_deck = Card.full_deck()
+    return [card for card in full_deck if card not in known_cards]
 
-def remove_cards(deck, used_cards):
-    return [card for card in deck if card not in used_cards]
 
-def get_hand_from_range(hand_range, deck):
-    while True:
-        hand_str = random.choice(hand_range)
-        r1, r2 = hand_str[0], hand_str[1]
-        suited = 's' in hand_str
-        offsuit = 'o' in hand_str
-        candidates = []
+def evaluate_hand(hand, community, evaluator):
+    return evaluator.evaluate(hand, community)
 
-        for c1 in deck:
-            for c2 in deck:
-                if c1 == c2:
-                    continue
-                if c1[0] == r1 and c2[0] == r2 or c1[0] == r2 and c2[0] == r1:
-                    if suited and c1[1] == c2[1]:
-                        candidates.append((c1, c2))
-                    elif offsuit and c1[1] != c2[1]:
-                        candidates.append((c1, c2))
-                    elif not suited and not offsuit and c1[0] == c2[0]:
-                        candidates.append((c1, c2))
-        if candidates:
-            return random.choice(candidates)
+
+def generate_all_hole_card_combinations():
+    ranks = "23456789TJQKA"
+    suits = "shdc"
+    deck = [r + s for r in ranks for s in suits]
+    combos = list(itertools.combinations(deck, 2))
+    return combos
